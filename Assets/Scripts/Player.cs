af7457e753ef1f2b14cff576a719db9c78a0391b
Lekhman-vold <volodya.l@yahoo.com>
2021-07-13 22:32:14 +0300

    first commit

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+            rb.AddForce(Vector3.up * 400f * 0.02f, ForceMode.Impulse);
+    }
+}
